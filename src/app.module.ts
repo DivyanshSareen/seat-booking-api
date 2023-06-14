03febee4f6581206503de7d943358e1253d2f671
DivyanshSareen <divyansh@LAPTOP-MNECQT85.localdomain>
2023-06-14 19:20:29 +0530

    Intialized seat pricing and booking modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SeatModule } from './seat/seat.module';
+import { PricingModule } from './pricing/pricing.module';
+import { BookingModule } from './booking/booking.module';
 
 @Module({
-  imports: [],
+  imports: [SeatModule, PricingModule, BookingModule],
   controllers: [AppController],
   providers: [AppService],
 })
